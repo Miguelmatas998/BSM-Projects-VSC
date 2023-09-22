cdd43f11e2a486131e27768d89bb3a579e66fd99
Miki <miguelmatas998@gmail.com>
2023-09-22 13:27:25 +0200

    Actividad L18

---
+++ L18/src/app/tab1/tab1.page.ts
@@ -0,0 +1,218 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+import { AlertController } from '@ionic/angular';
+
+import * as Mnemonic from 'bitcore-mnemonic';
+import Web3 from 'web3';
+import * as CryptoJS from 'crypto-js';
+import { WalletService } from './../wallet.service';
+import { ABI as ICOBSM } from './../ICOBSM';
+import * as util from '@ethereumjs/util';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  window: any;
+
+  encrypted: any;
+
+  wallet:any = {
+    address: ""
+  };
+
+  loginForm: any;
+
+  icoForm: any;
+
+  tokensForm: any;
+
+  lastTransaction: any;
+
+  web3: any;
+
+  // ICO CONTRACT
+  icoContract: any;
+
+  icoContractAddress: any = '0x526ea39cbdd24c7fae51f5fa99afb815a678783c';
+
+  icoBalanceOfTokens: any;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private formBuilder: FormBuilder,
+    private walletService: WalletService,
+    private alertController: AlertController
+  ) {
+    this.window = document.defaultView;
+
+    // LOGIN
+    this.encrypted = window.localStorage.getItem('seeds');
+
+    this.loginForm = formBuilder.group({
+      seeds: "",
+      password: ""
+    });
+
+    // BUY ICO
+    this.icoForm = formBuilder.group({
+      amount: ""
+    });
+
+    // SEND TOKENS
+    this.tokensForm = formBuilder.group({
+      address: "",
+      amount: ""
+    });
+
+    // INIT WEB3
+    this.web3 = new Web3;
+
+    this.web3.setProvider(
+      new this.web3.providers.HttpProvider('https://sepolia.infura.io/v3/d09825f256ae4705a74fdee006040903')
+    );
+
+    // ICO CONTRACT
+    this.icoContract = new this.web3.eth.Contract(ICOBSM.default, this.icoContractAddress);
+  }
+
+  async sendLogin(sendData:any) {
+    if (!sendData.password) {
+      const alert = await this.alertController.create({
+        message: 'Campos obligatorios',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+      return;
+    }
+
+    if (this.encrypted) {
+      var semillas = CryptoJS.AES.decrypt(this.encrypted, sendData.password).toString(CryptoJS.enc.Utf8);
+      sendData.seeds = semillas;
+    }
+
+    if (!Mnemonic.isValid(sendData.seeds)) {
+      const alert = await this.alertController.create({
+        message: 'Semilla inválida',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+      return;
+    }
+
+    this.encrypted = CryptoJS.AES.encrypt(sendData.seeds, sendData.password);
+
+    window.localStorage.setItem('seeds', this.encrypted.toString());
+
+    this.wallet = await this.walletService.initWallet(sendData.seeds);
+    this.icoBalanceOfTokens = await this.icoContract.methods.balanceOf(this.wallet.address).call();
+  }
+
+  loginMetamask() {
+    this.window.ethereum.enable().then(async (accounts:any) => {
+      this.wallet.address = accounts[0];
+    });
+  }
+
+  async sendIco(sendData:any) {
+    if (!sendData.amount) {
+      const alert = await this.alertController.create({
+        message: 'Introduce un importe',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+      return;
+    }
+
+    // amount = 1000000000000000
+    var rawData = {
+      from: this.wallet.address,
+      to: this.icoContractAddress,
+      value: sendData.amount,
+      gasPrice: this.web3.utils.toHex(10000000000),
+      gasLimit: this.web3.utils.toHex(1000000),
+      nonce: await this.web3.eth.getTransactionCount(this.wallet.address),
+    };
+
+    var signed = await this.web3.eth.accounts.signTransaction(rawData, this.wallet.privateKey.toString('hex'));
+
+    this.web3.eth.sendSignedTransaction(signed.rawTransaction).then(
+      (receipt: any) => {
+        console.log(receipt)
+      },
+      (error: any) => {
+        console.log(error)
+      }
+    );
+  }
+
+  async sendTokens(sendData:any) {
+    if ( ! util.isValidAddress(sendData.address)) {
+      const alert = await this.alertController.create({
+        message: 'Dirección no válida',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+      return;
+    }
+
+    if (sendData.amount == '') {
+      const alert = await this.alertController.create({
+        message: 'Campos obligatorios',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+      return;
+    }
+
+    if (sendData.amount > this.icoBalanceOfTokens) {
+      const alert = await this.alertController.create({
+        message: 'No tienes suficientes tokens',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+
+      return;
+    }
+
+    var rawData = {
+      from: this.wallet.address,
+      to: this.icoContractAddress,
+      value: 0,
+      gasPrice: this.web3.utils.toHex(10000000000),
+      gasLimit: this.web3.utils.toHex(1000000),
+      nonce: await this.web3.eth.getTransactionCount(this.wallet.address),
+      data: this.icoContract.methods.transfer(sendData.address, sendData.amount).encodeABI()
+    };
+
+    var signed = await this.web3.eth.accounts.signTransaction(rawData, this.wallet.privateKey.toString('hex'));
+
+    this.web3.eth.sendSignedTransaction(signed.rawTransaction).then(
+        (receipt: any) => {
+          this.lastTransaction = receipt;
+        },
+        (error: any) => {
+            console.log(error)
+        }
+    );
+  }
+
+}
